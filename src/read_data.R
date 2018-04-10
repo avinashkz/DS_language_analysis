a17385048d7b7109c333e6e2ebfd559ed53ef708
ap26793 <zi.avi7@gmail.com>
2018-04-09 21:19:14 -0700

    Wrapped the code in function and added documentation

--- src/read_data.R
+++ src/read_data.R
@@ -1,9 +1,18 @@
 suppressPackageStartupMessages(library("googlesheets"))
 suppressPackageStartupMessages(library("dplyr"))
 suppressPackageStartupMessages(library("readr"))
+
+
+main <- function() {
+
 my_sheets <- gs_ls()
 title <- gs_title(my_sheets$sheet_title)
 responses <- gs_read(title)
 write_csv(x = responses, path = "data/survery_responses.csv")
 names(responses) <- c("timestamp", "agreement", "academics", "experience", "attitude", "first_language", "preference", "task", "active_use")
 write_csv(x = responses, path = "docs/survery_responses.csv")
+
+}
+
+# call main function
+main()
