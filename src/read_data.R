eb385f6acf5e7a10f026590495c7db29cfbafe79
ap26793 <zi.avi7@gmail.com>
2018-04-09 21:21:01 -0700

    Wrapped the code in function and added documentation

--- src/read_data.R
+++ src/read_data.R
@@ -4,7 +4,9 @@ suppressPackageStartupMessages(library("readr"))
 
 
 main <- function() {
-
+#Reads the data in from google sheets
+#Stores raw data in data folder
+#Stores the processed data in docs folder
 my_sheets <- gs_ls()
 title <- gs_title(my_sheets$sheet_title)
 responses <- gs_read(title)
