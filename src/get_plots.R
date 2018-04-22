ef662760e598c78ae525a3c6776306654898f5c6
ap26793 <zi.avi7@gmail.com>
2018-04-21 19:15:43 -0700

    Handled errors

--- src/get_plots.R
+++ src/get_plots.R
@@ -5,20 +5,46 @@ suppressPackageStartupMessages(library("cowplot"))
 theme_set(theme_bw())
 
 
-pie_plot <- function(x, i, responses, plot_name){
-  plot_colors <- c("red", "blue4")
-  colnames(x) <- c("ax1", "Class")
-  n <- n_distinct(x[,1])
+pie_chart <- function(data, name){
   
-  #Pie plots
-  p <-  x %>% group_by(ax1) %>%
+  # 
+  #
+  # Args:
+  #   data: One of two vectors whose sample covariance is to be calculated.
+  #   name: Name of the 
+  #
+  # Returns:
+  #   A pie chart for the given variable.
+  
+  if(missing(data)){
+    stop('argument "data" is missing, with no default')
+  }
+  
+  if(missing(name)){
+    stop('argument "name" is missing, with no default')
+  }
+  
+  if(!is.data.frame(data)){
+    stop('data input should be of type data frame')
+  }
+  
+  if(!is.character(name)){
+    stop('name input should be of type character')
+  }
+  
+  
+  plot_colors <- c("red", "blue4")
+  colnames(data) <- c("ax1", "Class")
+  n <- n_distinct(data[,1])
+  
+  p <-  data %>% group_by(ax1) %>%
     summarise(count = n()) %>% arrange(desc(ax1)) %>% 
     mutate(pos = cumsum(count) - count/2) %>% ggplot() +
     geom_bar(aes(x = "",
                  y = count, fill = ax1), alpha = 0.7, color = "black", stat = "identity") +
     geom_text(aes(x="", y=pos, label = count), size=5) +
-    xlab(responses[i]) + 
-    labs(title = paste("Distribution of" ,responses[i]), fill = "", x="", y="Number of people") +
+    xlab(name) + 
+    labs(title = paste("Distribution Of" ,name), fill = "", x="", y="Number of people") +
     scale_fill_brewer(palette = "Set1") +
     coord_polar("y", start=0) +
     theme_bw() + theme(plot.title = element_text(hjust = 0.5, size = 16), 
@@ -28,52 +54,65 @@ pie_plot <- function(x, i, responses, plot_name){
   return(p)
 }
 
-proportion_plot <- function(x, i, responses, plot_name){
-  plot_colors <- c("red", "blue4")
-  colnames(x) <- c("ax1", "Class")
-  n <- n_distinct(x[,1])
+# proportion_plot <- function(x, i, responses){
+#   plot_colors <- c("red", "blue4")
+#   colnames(x) <- c("ax1", "Class")
+# 
+#   #Proportion plots
+#   q <- x %>% group_by(ax1) %>% mutate(count = n()) %>%
+#     ggplot(aes(fct_reorder(ax1,count, .desc = TRUE))) +
+#     geom_bar(aes(y = (..count..)/sum(..count..), fill = Class), position = "fill", color = "black", alpha = 0.7) +
+#     xlab(responses[i]) +
+#     labs(title = paste("Proportions For" ,responses[i]), y = "Proportion") +
+#     scale_fill_manual(values = plot_colors)
+#     theme_bw() + theme(plot.title = element_text(hjust = 0.5, size = 12),
+#                        axis.text.x = element_text(angle=45, vjust=0.5, size = 12))
+#     
+#     return(q)
+# }
 
-  #Proportion plots
-  q <- x %>% group_by(ax1) %>% mutate(count = n()) %>%
-    ggplot(aes(fct_reorder(ax1,count, .desc = TRUE))) +
-    geom_bar(aes(y = (..count..)/sum(..count..), fill = Class), position = "fill", color = "black", alpha = 0.7) +
-    xlab(responses[i]) +
-    labs(title = paste("Proportions for" ,responses[i]), y = "Proportion") +
-    scale_fill_manual(values = plot_colors)
-    theme_bw() + theme(plot.title = element_text(hjust = 0.5, size = 16),
-                       axis.text = element_text(size = 12),
-                       axis.title = element_text(size = 14))
 
-  if (n > 3){
-    return(q + coord_flip())
-  } else {
-    return(q)
+bar_plot <- function(data, name){
+  # Computes the sample covariance between two vectors.
+  #
+  # Args:
+  #   x: One of two vectors whose sample covariance is to be calculated.
+  #   y: The other vector. x and y must have the same length, greater than one,
+  #      with no missing values.
+  #   verbose: If TRUE, prints sample covariance; if not, not. Default is TRUE.
+  #
+  # Returns:
+  #   The sample covariance between x and y.
+  if(missing(data)){
+    stop('argument "data" is missing, with no default')
   }
-}
-
-
-bar_plot <- function(x, i, responses, plot_name, flip = TRUE){
-  plot_colors <- c("red", "blue4")
-  colnames(x) <- c("ax1", "Class")
   
-  #Bar plot
-  p <-  x %>% group_by(ax1, Class) %>%
+  if(missing(name)){
+    stop('argument "name" is missing, with no default')
+  }
+  
+  if(!is.data.frame(data)){
+    stop('data input should be of type data frame')
+  }
+  
+  if(!is.character(name)){
+    stop('name input should be of type character')
+  }
+  
+  
+  plot_colors <- c("red", "blue4")
+  colnames(data) <- c("ax1", "Class")
+  
+  p <-  data %>% group_by(ax1, Class) %>%
     summarise(count = n()) %>% ggplot() +
     geom_col(aes(x = fct_reorder(ax1,count, .desc = TRUE),
                  y = count, fill = Class), alpha = 0.7, color = "black") +
-    xlab(responses[i]) + 
-    labs(title = paste("Bar Plot for" ,responses[i]), y = "Count") +
+    xlab(name) + 
+    labs(title = paste(name), y = "Count") +
     scale_fill_manual(values = plot_colors) +
-    theme_bw() + theme(plot.title = element_text(hjust = 0.5, size = 16), 
-                       axis.text = element_text(size = 12), 
-                       axis.title = element_text(size = 14))
-
+    theme_bw() + theme(plot.title = element_text(hjust = 0.5, size = 12),
+                       axis.text.x = element_text(angle=45, vjust=0.5, size = 12))
   
-  if (flip){
-    return(p + coord_flip())
-  } else {
     return(p)
-  }
-  
 }
 
